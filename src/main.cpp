0657dab54b2daad8ea103593d8d73c7846f9ae92
Yuri K. Schlesner <yuriks@yuriks.net>
2013-01-02 20:19:08 -0500

    Moved some OpenGL code to a separate file.

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,7 @@
 #include "Fixed.hpp"
 #include "SpriteBuffer.hpp"
 #include "vec2.hpp"
-
-#define CHECK_GL_ERROR assert(glGetError() == GL_NO_ERROR)
+#include "graphics_init.hpp"
 
 std::vector<Sprite> debug_sprites;
 
@@ -27,109 +26,6 @@ void debugPoint(int x, int y) {
 	spr.y = static_cast<float>(y - 2);
 
 	debug_sprites.push_back(spr);
-}
-
-GLuint loadMainTexture(int* width, int* height) {
-	GLuint main_texture;
-	glGenTextures(1, &main_texture);
-
-	glBindTexture(GL_TEXTURE_2D, main_texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	int comp;
-	unsigned char* data = stbi_load("graphics.png", width, height, &comp, 4);
-	if (data == nullptr)
-		return 0;
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, *width, *height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
-	return main_texture;
-}
-
-GLuint loadShader(const char* shader_src, GLenum shader_type) {
-	GLuint shader = glCreateShader(shader_type);
-	glShaderSource(shader, 1, &shader_src, nullptr);
-	glCompileShader(shader);
-
-	GLint compile_result;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_result);
-
-	if (compile_result == GL_FALSE) {
-		GLint log_size;
-		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_size);
-
-		std::vector<char> log_buf(log_size);
-		glGetShaderInfoLog(shader, log_buf.size(), nullptr, log_buf.data());
-
-		std::cerr << "Error compiling shader:\n";
-		std::cerr << log_buf.data() << std::flush;
-
-		glDeleteShader(shader);
-		return 0;
-	}
-
-	return shader;
-}
-
-std::string loadTextFile(const std::string& filename) {
-	std::string ret;
-	
-	std::ifstream f(filename);
-	if (f) {
-		f.seekg(0, std::ios::end);
-		ret.resize(static_cast<size_t>(f.tellg()));
-		f.seekg(0);
-		f.read(&ret[0], ret.size());
-	}
-
-	return ret;
-}
-
-GLuint loadShaderProgram() {
-	std::string vertex_shader_src = loadTextFile("vertex_shader.glsl");
-	assert(!vertex_shader_src.empty());
-	std::string fragment_shader_src = loadTextFile("fragment_shader.glsl");
-	assert(!fragment_shader_src.empty());
-
-	GLuint vertex_shader = loadShader(vertex_shader_src.c_str(), GL_VERTEX_SHADER);
-	assert(vertex_shader != 0);
-	GLuint fragment_shader = loadShader(fragment_shader_src.c_str(), GL_FRAGMENT_SHADER);
-	assert(fragment_shader != 0);
-
-	GLuint program = glCreateProgram();
-	glAttachShader(program, vertex_shader);
-	glAttachShader(program, fragment_shader);
-	glLinkProgram(program);
-
-	GLint link_result;
-	glGetProgramiv(program, GL_LINK_STATUS, &link_result);
-
-	if (link_result == GL_FALSE) {
-		GLint log_size;
-		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_size);
-
-		std::vector<char> log_buf(log_size);
-		glGetProgramInfoLog(program, log_buf.size(), nullptr, log_buf.data());
-
-		std::cerr << "Error linking shader program:\n";
-		std::cerr << log_buf.data() << std::flush;
-
-		glDeleteProgram(program);
-		return 0;
-	}
-
-	return program;
-}
-
-void APIENTRY debug_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam)
-{
-	if ((type != GL_DEBUG_TYPE_PERFORMANCE_ARB && type != GL_DEBUG_TYPE_OTHER_ARB) || severity != GL_DEBUG_SEVERITY_LOW_ARB)
-		std::cerr << message << std::endl;
-	if ((type != GL_DEBUG_TYPE_PERFORMANCE_ARB && type != GL_DEBUG_TYPE_OTHER_ARB) || severity == GL_DEBUG_SEVERITY_HIGH_ARB)
-		__debugbreak(); // Breakpoint
 }
 
 struct Ball {
@@ -307,36 +203,12 @@ void collideBallWithPaddle(Ball& ball, const Paddle& paddle) {
 }
 
 int main() {
-	if (!glfwInit()) {
+	if (!initWindow(WINDOW_WIDTH, WINDOW_HEIGHT)) {
 		return 1;
-	}
-
-	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
-	glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
-	if (!glfwOpenWindow(WINDOW_WIDTH, WINDOW_HEIGHT, 8, 8, 8, 0, 0, 8, GLFW_WINDOW)) {
-		return 1;
-	}
-
-	if (gl3wInit()) {
-		return 1;
-	}
-
-	if (!gl3wIsSupported(3, 3)) {
-		return 1;
-	}
-
-	glfwSwapInterval(1);
-	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-	if (glDebugMessageCallbackARB) {
-		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
-		glDebugMessageControlARB(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, GL_TRUE);
-		glDebugMessageCallbackARB(debug_callback, 0);
 	}
 
 	int tex_width, tex_height;
-	GLuint main_texture = loadMainTexture(&tex_width, &tex_height);
+	GLuint main_texture = loadTexture(&tex_width, &tex_height, "graphics.png");
 	assert(main_texture != 0);
 
 	glEnable(GL_BLEND);
